0ce4c4b23259068a6cae6c7c7d620c38ac7c38c3
Ruslan Prokopenko <rus@lmg.kiev.ua>
2016-06-16 20:40:07 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+The app implements requirements for Fee Code Camp [File Metadata Microservice project](https://www.freecodecamp.com/challenges/file-metadata-microservice).
+
+Running app can be found [here](https://fileweighter.herokuapp.com/).
---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require("express"),
+    app = express(),
+    multer = require("multer"),
+    upload = multer(),
+    path = require("path");
+    
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname,'pages','index.html'));
+});
+
+app.post('/post', upload.single('file'), function (req, res, next) {
+  if (req.file)
+    res.json({"size": req.file.size});
+  else
+    res.json({'error':'no file received'});
+});
+
+app.use(function(req, res){
+  res.status(404);
+  res.sendFile(path.join(__dirname,'pages','notfound.html'));
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500)
+  res.sendFile(path.join(__dirname,'pages','servererror.html'));
+});
+
+app.listen( process.env.port || 8080, function(){
+  console.log('express started');
+});
